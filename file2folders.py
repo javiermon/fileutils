314b298d8ed3b7f5a86afc9b931edefbbcf70cbd
Javier <javiermon@gmail.com>
2012-07-08 13:48:15 +0200

    first take

--- file2folders.py
+++ file2folders.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import sys, os
+import sys, os, shutil
 import optparse
 import logging
 from logging import handlers
@@ -9,14 +9,29 @@ FULLFORMAT = "%(asctime)s  [%(levelname)s]  [%(module)s] %(message)s"
 logger = logging.getLogger()
 delimiters = ". _"
 
-def file2folder(directory, simulate):
-    for entry in os.
+def file2folder(directory, rename=False, simulate=False):
+    for dirname, _, filenames in os.walk(directory):
+        for filename in filenames:
+            fullfile = os.path.join(dirname, filename)
+            name, extension = os.path.splitext(filename)
+            for delimiter in delimiters:
+                name = name.replace(delimiter, ".")
+            subdirname = os.path.join(dirname, name)
+            logger.info("mkdir %s" % subdirname)
+            if not simulate:
+                os.mkdir(subdirname)
+
+            if rename:
+                subdirname = "%s/%s" % (subdirname, os.path.join(name, extension))
+            logger.info("mv %s %s" % (fullfile, subdirname))
+            if not simulate:
+                shutil.move(fullfile, subdirname)
 
 def main():
     # Setup the command line arguments.
     optp = optparse.OptionParser()
     # options.
-    optp.add_option("-d", "--directory", dest="dir",
+    optp.add_option("-d", "--directory", dest="directory",
                     help="directory to parse.")
 
     optp.add_option("-v", "--verbose", dest="verbose",
@@ -24,6 +39,9 @@ def main():
 
     optp.add_option("-s", "--simulate", dest="simulate",
                     help="do nothing, just simulate.", action="store_true", default=False)
+
+    optp.add_option("-r", "--rename", dest="rename",
+                    help="rename the file changing delimiters.", action="store_true", default=False)
 
     opts, args = optp.parse_args()
 
@@ -37,6 +55,10 @@ def main():
                         format=FULLFORMAT)
 
     logger.debug("init")
+    if opts.directory is None:
+        opts.directory = os.getcwd()
+    file2folder(opts.directory, opts.rename, opts.simulate)
+        
 
 if __name__ == "__main__":    
     main()
