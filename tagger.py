1ae686119edd0d264561eb3889315603601ea793
Javier <javiermon@gmail.com>
2012-12-30 20:29:55 +0100

    fixes for cmd tagger & % operand

--- tagger.py
+++ tagger.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python                                                                                                                                                         
+#!/usr/bin/python
 import sys, os
 import logging
 import optparse
@@ -14,19 +14,19 @@ def filetagger(directory, simulate=False):
         if os.path.isfile(os.path.join(directory, filename)):
             name, extension = os.path.splitext(filename)
             if extension in extensions:
-                logger.debug("tagging %s%s" % name, extension)
+                logger.debug("tagging %s%s" % (name, extension))
                 song = name[3:]
                 number = name[:2]
                 path = os.getcwd().split('/')
                 artist = path[-2]
                 album = path[-1]
-                cmd = "id3v2 -a %s -A %s -t '%s' -T %s '%s'" % (artist, album, song, number, filename)
+                cmd = "id3v2 -a '%s' -A '%s' -t '%s' -T '%s' '%s'" % (artist, album, song, number, filename)
                 logger.debug(cmd)
                 if not simulate:
                     os.system(cmd)
 
 def main():
-    # Setup the command line arguments.                                                                                                                                   
+    # Setup the command line arguments.
     optp = optparse.OptionParser()
     # options.
 
@@ -40,7 +40,7 @@ def main():
                     help="do nothing, just simulate.", action="store_true", default=False)
 
     opts, _ = optp.parse_args()
-    
+
     loglevel = logging.DEBUG if opts.verbose else logging.INFO
     logformat = FULLFORMAT if opts.verbose else BASICFORMAT
     # log to stderr in fg
