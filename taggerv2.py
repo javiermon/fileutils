25cba7c00791411995e3d03b2c61b3c0cf7a1b8d
Javier <javiermon@gmail.com>
2013-01-26 17:14:52 +0100

    id3v1 to id3v2 massive file converter

---
+++ taggerv2.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+import sys, os
+import logging
+import optparse
+
+FULLFORMAT = "%(asctime)s  [%(levelname)s]  [%(module)s] %(message)s"
+BASICFORMAT = "%(message)s"
+extensions = (".mp3", ".ogg")
+logger = logging.getLogger()
+
+def filetagger(directory, simulate=False):
+    root = os.path.abspath(directory)
+    for filename in os.listdir(root):
+        if os.path.isfile(os.path.join(directory, filename)):
+            name, extension = os.path.splitext(filename)
+            if extension in extensions:
+                logger.debug("v2 tagging %s%s" % (name, extension))
+                cmd = "id3v2 -l  '%s' | grep 'No ID3v2 tag'" % (filename)
+                logger.debug(cmd)
+                if not simulate:
+                    ret = os.system(cmd)
+                    if ret == 0:
+                        cmd = "id3v2 -C  '%s'" % (filename)
+                        logger.debug(cmd)
+                        if not simulate:
+                            os.system(cmd)
+
+def main():
+    # Setup the command line arguments.
+    optp = optparse.OptionParser()
+    # options.
+
+    optp.add_option("-d", "--directory", dest="directory",
+                    help="directory to parse.")
+
+    optp.add_option("-v", "--verbose", dest="verbose",
+                    help="log verbosity.", action="store_true", default=False)
+
+    optp.add_option("-s", "--simulate", dest="simulate",
+                    help="do nothing, just simulate.", action="store_true", default=False)
+
+    opts, _ = optp.parse_args()
+
+    loglevel = logging.DEBUG if opts.simulate or opts.verbose else logging.INFO
+    logformat = FULLFORMAT if opts.verbose else BASICFORMAT
+    # log to stderr in fg
+    logging.basicConfig(level=loglevel,
+                        format=logformat)
+
+    if opts.directory is None:
+        print >> sys.stderr, "please specify a valid directory"
+        optp.print_help()
+        sys.exit(-1)
+
+    if not os.path.isdir(opts.directory):
+        print >> sys.stderr, "%s is not a valid directory" % opts.directory
+        sys.exit(-1)
+
+    for root, _, _ in os.walk(opts.directory):
+        filetagger(root, opts.simulate)
+
+if __name__ == "__main__":
+    main()
